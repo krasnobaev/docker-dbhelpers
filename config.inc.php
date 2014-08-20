c03fe1b4a78105f3190a7702f7ff4144719739d3
krasnobaev <alekseykrasnobaev@gmail.com>
2014-08-20 23:07:44 +0400

    automatically get mysql connection parameters

--- config.inc.php
+++ config.inc.php
@@ -4,15 +4,15 @@ $i = 0;
 
 /* Server: localhost [1] */
 $i++;
-$cfg['Servers'][$i]['verbose'] = 'localhost';
-$cfg['Servers'][$i]['host'] = '172.17.0.2';
-$cfg['Servers'][$i]['port'] = '';
+$cfg['Servers'][$i]['verbose'] = 'linkedMySQLcontainer';
+$cfg['Servers'][$i]['host'] = 'MYSQL_PORT_3306_TCP_ADDR';
+$cfg['Servers'][$i]['port'] = 'MYSQL_PORT_3306_TCP_PORT';
 $cfg['Servers'][$i]['socket'] = '';
-$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['connect_type'] = 'MYSQL_PORT_3306_TCP_PROTO';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = 'mysecretpassword';
+$cfg['Servers'][$i]['password'] = 'MYSQL_ENV_MYSQL_ROOT_PASSWORD';
 
 /* End of servers configuration */
 
